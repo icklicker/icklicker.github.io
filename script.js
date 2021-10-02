a1ac10119b1b63d9e042af6be7717e31ebf5cecb
icklicker <87597005+icklicker@users.noreply.github.com>
2021-10-02 12:48:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Погода в " + name;
+    document.querySelector(".city").innerText = "Погода В " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
@@ -51,4 +51,4 @@ document
     }
   });
 
-weather.fetchWeather("Denver");
+weather.fetchWeather("Krasnodar");
